7de38e63c7ee75a7fd9af11f76182fb34a44f8c8
Kronos180 <79479825+Kronos180@users.noreply.github.com>
2022-05-05 12:32:18 -0700

    Add files via upload

--- public/flipandchangeimage.js
+++ public/flipandchangeimage.js
@@ -1,21 +1,19 @@
 myarray = [];
-function changeImage(img) {
-
+function changeImage(img, mySecondIMG) {
     var image = img;
-    if (image.src.match("baby-yoda.jpeg")) {
-        image.src = "Evil dragon.jpg";
+    if (image.src.match("grey.jpg")) {
+        image.src = mySecondIMG;
     }
     else {
-        image.src = "baby-yoda.jpeg";
+        image.src = "grey.jpg";
     }
 }
 
 
-function flipImage(img) {
+function flipImage(img, mySecondIMG) {
     switch (img.dataset.flipped) {
         case 'true': img.style.transform = 'rotateY(0deg)'; img.dataset.flipped = 'false'; break;
         case 'false': img.style.transform = 'rotateY(180deg)'; img.dataset.flipped = 'true'; break;
     }
-    setTimeout(() => { changeImage(img) }, "200")
-
+    setTimeout(() => { changeImage(img, mySecondIMG) }, "200")
 }
